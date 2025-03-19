244efdf6e8852d7abfa68a5fc864e738f45efd95
jethrojyrwa <jethrojyrwaroy@gmail.com>
2025-03-19 21:58:04 +0530

    remvoed spacy

--- codeanalysis.py
+++ codeanalysis.py
@@ -1,15 +1,10 @@
 import streamlit as st
-import spacy
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
 
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+# Load spaCy model
+# nlp = spacy.load("en_core_web_sm")
 
 # Define C language components
 KEYWORDS = {
